29797b41d97c5ab991feb85877474f9dd82821f1
ianbylim <ianbylim@gmail.com>
2024-04-13 09:40:27 -0500

    updated template event_viewer.html and added map

--- ProtestTools/event_view.py
+++ ProtestTools/event_view.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import folium
 from app import app
 
 
 @app.route("/eventview")
 def view_events():
-    return render_template("event_viewer.html")
+    # set the iframe width and height
+    iframe_map = folium.Map(location=(38.9673769, -95.2793475))
+    iframe_map.get_root().width = "800px"
+    iframe_map.get_root().height = "600px"
+    iframe = iframe_map.get_root()._repr_html_()
+
+    return render_template("event_viewer.html", iframe=iframe)
