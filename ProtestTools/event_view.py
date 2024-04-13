003f2fffbb1686f29c5c24a78abf6c879cb85ebf
ianbylim <ianbylim@gmail.com>
2024-04-13 12:55:30 -0500

    commit to fix merge

--- ProtestTools/event_view.py
+++ ProtestTools/event_view.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Blueprint
 import folium
+from app import app
 
 # page contains list of created events and displays event information on click
 eventview = Blueprint("eventview", __name__)
@@ -16,3 +17,15 @@ def view_events():
     iframe = iframe_map.get_root()._repr_html_()
 
     return render_template("event_viewer.html", iframe=iframe)
+
+
+@eventview.route("/eventpage")
+def event_info():
+
+    # set the iframe width and height
+    iframe_map = folium.Map(location=(38.9673769, -95.2793475))
+    iframe_map.get_root().width = "800px"
+    iframe_map.get_root().height = "600px"
+    iframe = iframe_map.get_root()._repr_html_()
+
+    return render_template("default_event.html", iframe=iframe)
