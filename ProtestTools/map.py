a20dfa5f9fe8b6e8fe43a9a0ba3ad8bd080e55da
Discord-ian <Discord-ian@users.noreply.github.com>
2024-04-13 08:21:55 -0500

    Change order in createEvent form

--- ProtestTools/map.py
+++ ProtestTools/map.py
@@ -5,6 +5,18 @@ from app import app
 
 @app.route("/map/create_pin", methods=["GET", "POST"])
 def create_pin():
+    """
+    Form has fields:
+    event_name
+    event_date
+    event_time
+    event_address
+    event_city
+    event_zip
+    event_state
+    event_country
+    :return:
+    """
     if request.method == "GET":
         pass
     m = folium.Map(location=(38.9673769, -95.2793475))
