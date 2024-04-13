b3aadc226854aa76a67e8f1adf5776e89c0bcd83
ianbylim <ianbylim@gmail.com>
2024-04-13 14:53:59 -0500

    Changed some class names in event_viewer.html

--- ProtestTools/event_view.py
+++ ProtestTools/event_view.py
@@ -11,8 +11,8 @@ def view_events():
     # TODO: add links to individual event pages
     # set the iframe width and height
     iframe_map = folium.Map(location=(38.9673769, -95.2793475))
-    iframe_map.get_root().width = "800px"
-    iframe_map.get_root().height = "600px"
+    iframe_map.get_root().width = "600px"
+    iframe_map.get_root().height = "400px"
     iframe = iframe_map.get_root()._repr_html_()
 
     return render_template("event_viewer.html", iframe=iframe)
