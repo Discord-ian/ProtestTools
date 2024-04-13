cf3373c5caf2385fb2b098d292d29f4c7e30ac78
ianbylim <ianbylim@gmail.com>
2024-04-13 09:43:37 -0500

    added todo in event_view.py

--- ProtestTools/event_view.py
+++ ProtestTools/event_view.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 import folium
 from app import app
 
+# page contains list of created events and displays event information on click
+
 
 @app.route("/eventview")
 def view_events():
+    # TODO: add information to main from database on click
+    # TODO: add links to individual event pages
     # set the iframe width and height
     iframe_map = folium.Map(location=(38.9673769, -95.2793475))
     iframe_map.get_root().width = "800px"
