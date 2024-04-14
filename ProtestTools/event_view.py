76e94b9ebf12bc9130de445c210aca9fe2f7cfdb
Discord-ian <Discord-ian@users.noreply.github.com>
2024-04-14 02:08:40 -0500

    Add 12hr time

--- ProtestTools/event_view.py
+++ ProtestTools/event_view.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Blueprint, redirect, url_for
 from db import get_event_dicts, valid_event_id, get_event_info
+from datetime import datetime
 from flask_login import current_user, login_required
 import folium
 
@@ -18,6 +19,8 @@ def view_events():
     events = get_event_dicts()
     iframe_map = folium.Map(location=(38.9673769, -95.2793475))
     for event in events:
+        d = datetime.strptime(event["time"], "%H:%M")
+        event["twelve_hr"] = d.strftime("%I:%M %p")
         folium.Marker(
             location=(event["location"]["lat"], event["location"]["lng"]),
             popup=event["event_name"],
@@ -32,7 +35,10 @@ def view_events():
 @login_required
 def event_info(event_id):
     if valid_event_id(event_id):
+        twelve_hr = None
         event = get_event_info(event_id)
+        d = datetime.strptime(event["time"], "%H:%M")
+        twelve_hr = d.strftime("%I:%M %p")
         location = event["location"]
         address = (
             location["address"]
@@ -51,7 +57,9 @@ def event_info(event_id):
             location=(float(location["lat"]), float(location["lng"])), popup=address
         ).add_to(iframe_map)
         iframe = iframe_map.get_root()._repr_html_()
-        return render_template("default_event.html", iframe=iframe, event=event)
+        return render_template(
+            "default_event.html", iframe=iframe, event=event, twelve_hour=twelve_hr
+        )
     else:
         return redirect(url_for("eventview.view_events"))
     # set the iframe width and height
